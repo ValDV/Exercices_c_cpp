643793d49c5abd31b61fe212d91f81332edde81f
ValDV <valentinrsr20@gmail.com>
2024-01-29 23:20:22 +0100

    exercices

---
+++ TD N°1/exercice1.c
@@ -0,0 +1,9 @@
+int absolut (int valeur)
+{
+	If(valeur < 0)
+	{
+		valeur *= -1 ;
+	}
+
+    return valeur;
+}
---
+++ TD N°1/exercice2.c
@@ -0,0 +1,6 @@
+int puissance(int x, int y)
+{
+    int result =pow(x,y);
+    
+    return result;
+}
