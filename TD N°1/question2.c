c60aa5f09f8c2450b37a42af1a8d952efe1165ff
ValDV <valentinrsr20@gmail.com>
2024-01-29 23:55:27 +0100

    j'avais oublié une question chef

---
+++ TD N°1/question2.c
@@ -0,0 +1,6 @@
+//1. float fait_le(char c1, char c2, char c3);
+//2. void affiche (int A) ;
+
+//3.
+//a. int affiche_erreur(float err_nbr)   La fonction affiche_erreur retourne une valeur de type int et attends 1 paramètre de type float.
+//b. long lit_enreg(int rec_nbr,int size)  La fonction lit_enreg retoune une valeur de type long et attends 2 paramètres de types int.
