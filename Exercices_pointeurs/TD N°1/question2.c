7da1a95879eb0a8a3f11a0597a792b4d56e15219
ValDV <valentinrsr20@gmail.com>
2024-02-03 11:10:44 +0100

    td1 pointeur fini

---
+++ Exercices_pointeurs/TD N°1/question2.c
@@ -0,0 +1,10 @@
+/*
+a) *P+2 :  case pointée par P à laquelle on ajoute 2
+b) *(P+2) : case pointée par P+2
+c) &P+1 : adresse de P+1
+d) &A[4]-3 : valeur qui appartient a l'adresse de A[4] - 3
+e) A+3 : ajoute 3 a A 
+f) &A[7]-P : valeur a l'adresse 7 du tableau A à laquelle on soustrait ensuite l'adresse pointée par P
+g) P+(*P-10) : ajoute la valeur à l'adresse pointée par P où on soustrait 10 à P
+h) (P+(P+8)-A[7]) : valeur à l'adresse pointée par P où on ajoute la valeur à l'adresse pointée par P+8 à laquelle on soustrait A[7]
+*/
