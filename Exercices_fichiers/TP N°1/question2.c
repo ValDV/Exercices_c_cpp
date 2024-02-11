ce66e4f65a9e2afe535843532f0001c06ed3156c
ValDV <valentinrsr20@gmail.com>
2024-02-11 10:45:33 +0100

    question2 tp1

---
+++ Exercices_fichiers/TP N°1/question2.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main() {
+    FILE *fichierSource = fopen("source.txt", "r");
+
+    if (fichierSource == NULL) {
+        fprintf(stderr, "Erreur lors de l'ouverture du fichier source.txt en lecture.\n");
+        return 1;
+    }
+
+    FILE *fichierDest = fopen("dest1.txt", "w");
+
+    if (fichierDest == NULL) {
+        fprintf(stderr, "Erreur lors de l'ouverture du fichier dest1.txt en écriture.\n");
+        return 1; 
+    }
+
+    int caractere;
+    while ((caractere = getc(fichierSource)) != EOF) {
+        putc(caractere, fichierDest);
+    }
+
+    fclose(fichierSource);
+    fclose(fichierDest);
+
+    return 0;
+}
