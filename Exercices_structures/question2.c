deae6e5d40d931a63f1e292108376535d7eeadf9
ValDV <valentinrsr20@gmail.com>
2024-02-12 15:23:25 +0100

    question2 fichiers

---
+++ Exercices_structures/question2.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+struct Point {
+    float x;
+    float y;
+};
+
+int main() {
+    struct Point points[15];
+
+    printf("Saisir les coordonnées des 15 points :\n");
+    for (int i = 0; i < 15; ++i) {
+        printf("Point %d :\n", i + 1);
+        printf("Saisir la coordonnée x : ");
+        scanf("%f", &points[i].x);
+        printf("Saisir la coordonnée y : ");
+        scanf("%f", &points[i].y);
+    }
+
+    printf("\nCoordonnées des points :\n");
+    for (int i = 0; i < 15; ++i) {
+        printf("Point %d : (%.2f, %.2f)\n", i + 1, points[i].x, points[i].y);
+    }
+
+    return 0;
+}
