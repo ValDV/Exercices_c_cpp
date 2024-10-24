1d9ebbb56f792dcd4286f08bf58c33f2e4c4e310
ValDV <valentinrsr20@gmail.com>
2024-10-24 09:19:57 +0200

    NombreComplexe update1

--- NombreComplexe/NombreComplexe/NombreComplexe.cpp
+++ NombreComplexe/NombreComplexe/NombreComplexe.cpp
@@ -8,13 +8,36 @@ NombreComplexe::NombreComplexe(double partieReelle, double partieImaginaire)
 
 void NombreComplexe::affiche()
 {
-	std::cout << PartieReelle << "+" << PartieImaginaire << "i";
+	if (this->PartieReelle !== 0) std::cout << this->partieReelle;
+	if (this->PartieImaginaire < 0) std::cout << this->partieImaginaire;
+	if (this->PartieImaginaire > 0) std::cout << this->partieImaginaire;
+	std::cout << >> std::endl;
 }
 
 NombreComplexe* NombreComplexe::saisie()
 {
-	NombreComplexe* result;
+	double PartieReelle, PartieImaginaire;
+	std::cout << "Entrer la partie réelle :" >> std::endl;
+	std::cin >> PartieReelle;
+	std::cout << "Entrer la partie imaginaire :" >> std::endl;
+	std::cin >> PartieImaginaire;
 
+	return new NombreComplexe(PartieReelle, PartieImaginaire);
+}
+
+NombreComplexe NombreComplexe::addition(NombreComplexe& b)
+{
+	this->PartieReelle += b.partieReelle;
+	this->PartieImaginaire += b.partieImagianire;
+	NombreComplexe result(this->PartieReelle, this->PartieImaginaire);
 	return result;
 }
 
+NombreComplexe NombreComplexe::multiplication(NombreComplexe& b)
+{
+	this->PartieReelle = this->PartieReelle * b.partieReelle - this->PartieImaginaire * b.partieImaginaire;
+	this->PartieImagianire = this->PartieReelle * b.partieImaginaire + this->PartieImaginaire * b.partieReelle;
+
+	NombreComplexe result(this->PartieReelle, this->PartieImaginaire);
+	return result;
+}
