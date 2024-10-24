cc3848d018928ed8f0739e77c9a10c501b4a4a86
ValDV <valentinrsr20@gmail.com>
2024-10-24 10:29:01 +0200

    NombreComplexe update2

--- NombreComplexe/NombreComplexe/NombreComplexe.cpp
+++ NombreComplexe/NombreComplexe/NombreComplexe.cpp
@@ -1,43 +1,70 @@
-#include "NombreComplexe.h"
+#include <iostream>
 
+class NombreComplexe {
+private:
+    double PartieReelle;
+    double PartieImaginaire;
+
+public:
+    NombreComplexe(double partieReelle, double partieImaginaire);
+
+    void affiche();
+    static NombreComplexe* saisie();
+    NombreComplexe addition(NombreComplexe& b);
+    NombreComplexe multiplication(NombreComplexe& b);
+};
+
+int main()
+{
+    NombreComplexe* complexe = NombreComplexe::saisie();
+    complexe->affiche();
+    NombreComplexe autreComplexe(2.0, 3.0);
+    NombreComplexe somme = complexe->addition(autreComplexe);
+    somme.affiche();
+    NombreComplexe produit = complexe->multiplication(autreComplexe);
+    produit.affiche();
+
+    delete complexe;
+
+    return 0;
+}
 NombreComplexe::NombreComplexe(double partieReelle, double partieImaginaire)
 {
-	this->PartieReelle = partieReelle;
-	this->PartieImaginaire = partieImaginaire;
+    this->PartieReelle = partieReelle;
+    this->PartieImaginaire = partieImaginaire;
 }
 
 void NombreComplexe::affiche()
 {
-	if (this->PartieReelle !== 0) std::cout << this->partieReelle;
-	if (this->PartieImaginaire < 0) std::cout << this->partieImaginaire;
-	if (this->PartieImaginaire > 0) std::cout << this->partieImaginaire;
-	std::cout << >> std::endl;
+    if (this->PartieReelle != 0) std::cout << this->PartieReelle;
+    if (this->PartieImaginaire < 0) std::cout << this->PartieImaginaire;
+    if (this->PartieImaginaire > 0) std::cout << "+" << this->PartieImaginaire;
+    std::cout << std::endl;
 }
 
 NombreComplexe* NombreComplexe::saisie()
 {
-	double PartieReelle, PartieImaginaire;
-	std::cout << "Entrer la partie réelle :" >> std::endl;
-	std::cin >> PartieReelle;
-	std::cout << "Entrer la partie imaginaire :" >> std::endl;
-	std::cin >> PartieImaginaire;
+    double PartieReelle, PartieImaginaire;
+    std::cout << "Entrer la partie reelle :" << std::endl;
+    std::cin >> PartieReelle;
+    std::cout << "Entrer la partie imaginaire :" << std::endl;
+    std::cin >> PartieImaginaire;
 
-	return new NombreComplexe(PartieReelle, PartieImaginaire);
+    return new NombreComplexe(PartieReelle, PartieImaginaire);
 }
 
 NombreComplexe NombreComplexe::addition(NombreComplexe& b)
 {
-	this->PartieReelle += b.partieReelle;
-	this->PartieImaginaire += b.partieImagianire;
-	NombreComplexe result(this->PartieReelle, this->PartieImaginaire);
-	return result;
+    double nouvellePartieReelle = this->PartieReelle + b.PartieReelle;
+    double nouvellePartieImaginaire = this->PartieImaginaire + b.PartieImaginaire;
+
+    return NombreComplexe(nouvellePartieReelle, nouvellePartieImaginaire);
 }
 
 NombreComplexe NombreComplexe::multiplication(NombreComplexe& b)
 {
-	this->PartieReelle = this->PartieReelle * b.partieReelle - this->PartieImaginaire * b.partieImaginaire;
-	this->PartieImagianire = this->PartieReelle * b.partieImaginaire + this->PartieImaginaire * b.partieReelle;
+    double nouvellePartieReelle = this->PartieReelle * b.PartieReelle - this->PartieImaginaire * b.PartieImaginaire;
+    double nouvellePartieImaginaire = this->PartieReelle * b.PartieImaginaire + this->PartieImaginaire * b.PartieReelle;
 
-	NombreComplexe result(this->PartieReelle, this->PartieImaginaire);
-	return result;
+    return NombreComplexe(nouvellePartieReelle, nouvellePartieImaginaire);
 }
